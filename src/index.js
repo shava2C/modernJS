9f942f9a704774ee40dd973bd8132c4cf977e173
shava2Curry <47783146+shava2C@users.noreply.github.com>
2021-02-25 15:58:51 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -82,14 +82,33 @@ const arr5 = [30, 40];
 // arr8[0] = 100;
 // console.log(arr4);
 
-const nameArr = ["田中", "山田", "安田"];
-// nameArr.map((name, index) => console.log(`${index}番目は${name}`));
+// const nameArr = ["田中", "山田", "安田"];
+// // nameArr.map((name, index) => console.log(`${index}番目は${name}`));
 
-const nameNewArr = nameArr.map((name) => {
-  if (name === "安田") {
-    return name;
-  } else {
-    return `${name}さん`;
-  }
-});
-console.log(nameNewArr);
+// const nameNewArr = nameArr.map((name) => {
+//   if (name === "安田") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(nameNewArr);
+
+/**
+ * 三項演算子
+ * if/elseを書く必要がなくなる
+ * ある条件を書いて「？」を書いて、trueの時：falseの時
+ */
+// const val1 = 1 < 0 ? "T" : "F";
+// console.log(val1);
+
+// const num = "1300";
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力して";
+// console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100超えてる" : "100以下";
+};
+console.log(checkSum(1, 100));
